e50e2033c6565decb33e19c36c11dc7aca58c678
aurte <aurt1austin@hotmail.comgit config --global user.name Fatalisz>
2020-01-08 17:02:38 +0700

    commit

---
+++ backend/dist/server.js
@@ -0,0 +1,20 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+require('dotenv').config();
+const app = express();
+const port = process.env.PORT || '5000';
+app.use(cors());
+app.use(express.json());
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri.toString(), { useNewUrlParser: true }).then(() => {
+    console.log(`mongo DB Ready to use`);
+}, err => {
+    console.log(`mongo DB Connection failed: ${err}`);
+});
+app.listen(port, () => {
+    console.log(`Server is running on port: ${port}`);
+});
+//# sourceMappingURL=server.js.map
