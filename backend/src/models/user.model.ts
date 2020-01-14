eb7a3d25492f4eebc8d1ed99473fccfb6d5fe450
aurte <aurt1austin@hotmail.comgit config --global user.name Fatalisz>
2020-01-14 17:43:20 +0700

    commit

---
+++ backend/src/models/user.model.ts
@@ -0,0 +1,19 @@
+import mongoose, {Schema, Document} from 'mongoose'
+
+export interface IUser extends Document {
+    username: String;
+}
+
+const userSchema = new Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+    minlength: 3
+  },
+}, {
+  timestamps: true,
+});
+
+module.exports = mongoose.model<IUser>('User', userSchema);
