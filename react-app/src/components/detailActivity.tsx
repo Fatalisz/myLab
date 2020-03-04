98b7be3e2c16251d4f8fd2d0d5567cc9425807b4
aurte <aurt1austin@hotmail.comgit config --global user.name Fatalisz>
2020-03-04 18:22:15 +0700

    add bootstrap

--- react-app/src/components/detailActivity.tsx
+++ react-app/src/components/detailActivity.tsx
@@ -4,10 +4,20 @@ import '../css/App.css';
 class DetailActivity extends React.Component {
     render() {
         return (
-            <div id="divDetailActivity" className="border-gray-light">
-                <div id="headerDetailActivity" className="w100per color-AIS w3-padding header-detail">
+            <div id="divDetailActivity">
+                <div id="headerDetailActivity" className="w100per color-AIS w3-padding header-detail border-gray-light">
                     <span className="font-bold">Detail Activity</span>
                     <span className="fa fa-caret-square-o-up font-white w3-right"></span>
+                </div>
+                <div id="bodyDetailActivity">
+                    <div className="w3-row">
+                        <div className="w3-col w50per">
+                            col1
+                        </div>
+                        <div className="w3-col w50per">
+                            col2
+                        </div>
+                    </div>
                 </div>
             </div>
         );
